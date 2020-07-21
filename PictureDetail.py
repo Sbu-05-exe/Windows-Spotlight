04616cfb0b6b4bcac0532bc5557ff210bc963d5d
Sibusiso <itsbudamain.05@gmail.com>
2020-07-21 13:13:20 +0200

    Enabled buttons to traverse images

--- PictureDetail.py
+++ PictureDetail.py
@@ -1,3 +1,4 @@
+import App
 import PyQt5, sys
 from PyQt5.QtWidgets import QWidget, QLabel, QListWidget
 from PyQt5.QtWidgets import QApplication, QPushButton, QVBoxLayout, QHBoxLayout
@@ -8,7 +9,7 @@ class PictureDisplay(QWidget):
 	def __init__(self):
 		super().__init__()
 		self.picture = None
-		setting_layout = self.create_layout()
+		setting_layout, self.btnBackward, self.btnForward = self.create_layout()
 		self.setLayout(setting_layout)
 		self.setPicture()
 		self.show()
@@ -20,6 +21,8 @@ class PictureDisplay(QWidget):
 		btnForward = QPushButton(self)
 		btnBackward = QPushButton(self)
 
+		# Making widgets reponsive
+
 		btnForward.setSizePolicy(QP.Preferred, QP.Expanding)
 		btnBackward.setSizePolicy(QP.Preferred, QP.Expanding)
 		self.picture.setSizePolicy(QP.Expanding, QP.Expanding)
@@ -27,27 +30,55 @@ class PictureDisplay(QWidget):
 		btnForward.setText('-->')
 		btnBackward.setText('<--')
 
+		btnForward.clicked.connect(lambda: self.nextpic(1))
+		btnBackward.clicked.connect(lambda :self.nextpic(-1) )
+
+		print(dir(btnBackward))
+
 		hbox.addWidget(btnBackward)
 		hbox.addWidget(self.picture)
 		hbox.addWidget(btnForward)
 
-		return hbox
+		return hbox, btnBackward, btnForward
+
+	def setFn(self):
+		self.setPicture
 
 	def setPicture(self):
-		pixmap = QPixmap('C:/Users/itsbu/Pictures/Wallpapers/Landscape/972941670f3f9c895c2ddbaf50e65fc608ce90e6153a40bdce13c83717942f26.jpg')
-		print(dir(pixmap))
+		pixmap = QPixmap(App.dirs['cd'] + App.activeImage)
+
+		index = (App.images.index(App.activeImage))
+
+		if index == 0:
+			self.btnBackward.setEnabled(False)
+			self.btnForward.setEnabled(True)
+
+		elif index == (len(App.images) -1):
+			self.btnForward.setEnabled(False)
+			self.btnBackward.setEnabled(True)
+
+		else:
+			self.btnForward.setEnabled(True)
+			self.btnBackward.setEnabled(True)
+
 		print('height:', pixmap.height())
 		print('width:', pixmap.height())
 
-		pixmap = pixmap.scaled(500, 500)
+		pixmap = pixmap.scaled(500, 300)
 		self.picture.setPixmap(pixmap)
+
+	def nextpic(self, forward=1):
+		index = (App.images.index(App.activeImage))
+		index = index + forward
+		App.activeImage = App.images[index]
+		self.setPicture()
 
 def main():
 	App = QApplication(sys.argv)
 	App.setApplicationName('Settings')
 
-	PictureDisplay = PictureDisplay()
-	PictureDisplay.show()
+	PicDisplay = PictureDisplay()
+	PicDisplay.show()
 	sys.exit(App.exec_())
 
 if __name__ == "__main__":
